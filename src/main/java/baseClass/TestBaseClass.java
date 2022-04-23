e597da8e99134b1b6afc6e9f0aa29e0f6b2c1a33
pooja <pooja_shettigar@perfaware.com>
2022-04-24 00:46:33 +0530

    test code

--- src/main/java/baseClass/TestBaseClass.java
+++ src/main/java/baseClass/TestBaseClass.java
@@ -4,11 +4,7 @@ import java.net.MalformedURLException;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
+import org.testng.annotations.*;
 
 import java.net.URL;
 
